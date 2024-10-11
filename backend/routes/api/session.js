7d79cb60ba91692422c66793e70128f0f44135c0
raihanhasan <your.email@susickjoarder@gmail.com>
2024-10-10 20:43:42 -0400

    added the fist name and last name atr

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -46,6 +46,8 @@ router.post(
 
     const safeUser = {
       id: user.id,
+      firstName: user.firstName,
+    lastName: user.lastName,
       email: user.email,
       username: user.username,
     };
@@ -81,6 +83,8 @@ router.get(
       if (user) {
         const safeUser = {
           id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
           email: user.email,
           username: user.username,
         };
